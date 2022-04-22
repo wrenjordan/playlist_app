30dc66a4c46146b163ec0a86ff150877796834f2
Jordan Wren <93288007+wrenjordan@users.noreply.github.com>
2022-04-21 22:31:15 -0400

    Created Static Components

---
+++ spotify_playlist/src/Components/App/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import SearchBar from '../SearchBar/SearchBar';
+import SearchResults from '../SearchResults/SearchResults';
+import Playlist from '../Playlist/Playlist';
+
+function App() {
+  return (
+    <div>
+      <h1>Ja<span className="highlight">mmm</span>ing</h1>
+      <div className="App">
+        {/* Add a SearchBar component */}
+        <SearchBar />
+        <div className="App-playlist">
+          {/* Add a SearchResults component */}
+          <SearchResults />
+          {/* Add a Playlist component */}
+          <Playlist />
+        </div>
+      </div>
+  </div>
+  );
+}
+
+export default App;
